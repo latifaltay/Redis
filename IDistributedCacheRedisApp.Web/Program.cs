b94d7b4c470f33d23fb9888c21c4d3f0feae7651
latifaltay <latifaltay00@gmail.com>
2025-08-15 05:48:52 +0300

    IDistributedCache registration

--- IDistributedCacheRedisApp.Web/Program.cs
+++ IDistributedCacheRedisApp.Web/Program.cs
@@ -3,6 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = "localhost:6379";
+}); 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
