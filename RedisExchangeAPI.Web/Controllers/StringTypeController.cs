9b2be1013db4e41574f1159f4100d7626534dfc7
latifaltay <latifaltay00@gmail.com>
2025-08-20 02:31:13 +0300

    String example

---
+++ RedisExchangeAPI.Web/Controllers/StringTypeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class StringTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase db;
+        public StringTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(0);
+        }
+
+        public IActionResult Index()
+        {
+
+            db.StringSet("name", "Latif Altay 54");
+            db.StringSet("Ziyaretçi", 100);
+
+            return View();
+        }
+
+
+        public IActionResult Show()
+        {
+            var value = db.StringGet("name");
+            db.StringIncrement("Ziyaretçi", 1);
+            if (value.HasValue) 
+            {
+                ViewBag.value = value.ToString();
+            }
+
+            return View();
+        }
+    }
+}
