b7af010b5366cf82ac532d68715454e467197615
latifaltay <latifaltay00@gmail.com>
2025-08-20 07:04:27 +0300

    test

---
+++ RedisExampleApp.API/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExampleApp.API.Models;
+using RedisExampleApp.API.Repository;
+
+namespace RedisExampleApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly IProductRepository _porductRepository;
+
+        public ProductsController(IProductRepository porductRepository)
+        {
+            _porductRepository = porductRepository; 
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() 
+        {
+            var value = await _porductRepository.GetAsync();
+            return Ok(value);
+        }
+
+        // www.api.com/products/10
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id) 
+        {
+            var value = await _porductRepository.GetByIdAsync(id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product) 
+        {
+            await _porductRepository.CreateAsync(product);
+            return Created();
+        }
+
+    }
+}
---
+++ RedisExampleApp.API/Repository/ProductRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RedisExampleApp.API.Models;
+
+namespace RedisExampleApp.API.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+
+        private readonly AppDbContext _context;
+
+        public ProductRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> CreateAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<List<Product>> GetAsync()
+        {
+            var products = await _context.Products.ToListAsync();
+            return products;
+        }
+
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            return product;
+        }
+    }
+}
