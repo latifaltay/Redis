e0179bb0cbda49086b70a361b450d6e26db5680a
latifaltay <latifaltay00@gmail.com>
2025-08-20 07:04:07 +0300

    Clear gitignore cache and some updates

--- IDistributedCacheRedisApp.Web/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IDistributedCacheRedisApp.Web.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Price { get; set; }
-    }
-}
